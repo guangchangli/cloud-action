d0bea759ad075a8a16d2ad11dff4b49a44a5b794
guangchang <guangchangde@gmail.com>
2020-07-24 01:00:03 +0800

    add redis starter

---
+++ redis-spring-boot-starter/src/test/java/com/aladdin/redis/springboot/starter/RedisSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aladdin.redis.springboot.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
